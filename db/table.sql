0018da9f2282e50ce8740857fceb926d0e422da3
Juan Espinoza <juanespinoza@Juans-MacBook-Air.local>
2019-08-03 14:52:05 -0700

    Database connection is set, we will need to import the pool in our future conrollers file where our middleware can connect to the db and store the results from the table into res.locals

---
+++ db/table.sql
@@ -0,0 +1,4 @@
+INSERT INTO thirty (firstname, lastname, email) values ('Jay','Dawson','dawsonjamar@gmail.com');
+INSERT INTO thirty (firstname, lastname, email) values ('Jake','Ory','jacobtory@yahoo.com');
+INSERT INTO thirty (firstname, lastname, email) values ('Taylor','Burrington','taylor.burrington@gmail.com');
+-- INSERT INTO thirty (firstname, lastname, email) values ('Jay','Dawson','dawsonjamar@gmail.com');
