bc5f021147fd88d292839ef93eb9f1f1dafea378
Juan Espinoza <juanespinoza@Juans-MacBook-Air.local>
2019-08-05 16:40:04 -0700

    Stlye changes, needs more updating. Implemented the logic to randomize four people from our data base. Those people are rendered on the page

--- server/mainController.js
+++ server/mainController.js
@@ -5,13 +5,10 @@ const middlewareFuncs = {};
 middlewareFuncs.addUser = (req, res, next) => {
     // values to be inserted into new, come from request body
     let values = [req.body.firstname, req.body.lastname, req.body.email];
-    console.log(values);
-    // need to fix this query to check for duplicates
-    //if not exists (select * from Delegates d where d.FromYr = @FromYr and d.MemNo = @MemNo)
-    //INSERT INTO Delegates ([MemNo],[FromYr],[ToYr]) values(@MemNo, @FromYr,@ToYr)
+    //console.log(values);
     pool.query(`INSERT INTO thirty (firstname, lastname, email) VALUES ($1, $2, $3);`,
       values, (err, results) => {
-        if(err) console.log('Need to take care of possible duplicate entries');
+        if(err) console.log('User is already created, or issues with the input fields');
         next();
     })
 }
