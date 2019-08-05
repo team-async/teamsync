722f96016243f5cb3841acfae3d4d24090c4c68a
Taylor Burrington <taylor.burrington@gmail.com>
2019-08-05 08:22:50 -0700

    DO NOT MERGE

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,19 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+
 
 // main component
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {}
-        // functions to bind later
+  constructor(props) {
+    super(props);
+    this.state = {isSignedIn=false};
+    // functions to bind later
+  }
 
-    }
+// componentDidMount(
 
-    render(){
-        return (
-            <h1>TEAM SYNC BOI!</h1>
-        )
-    }
+// )
+
+  render() {
+    return (
+        this.state.isSignedIn ? 
+        <Home/>
+        :
+        <div>Not Signed In!</div>
+       
+    );
+  }
 }
 
 export default App;
--- client/index.js
+++ client/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './components/app.jsx';
+import {Route, Link, BrowserRouter as Router, IndexRoute, Switch} from 'react-router-dom';
+import Home from './components/home.jsx'
 
 import './style/styles.css';
 
 render(
-    <App />, document.getElementById('root')
+    <Router>
+        <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path='/home' component={Home}/>
+        </Switch>
+     </Router>,
+     document.getElementById('root')
 );
